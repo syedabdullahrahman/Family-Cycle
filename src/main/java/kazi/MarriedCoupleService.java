f4e5907c52b718393b4045405a7d9042545226d3
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-07-25 18:10:38 +0600

    Married Couple added after problem

---
+++ src/main/java/kazi/MarriedCoupleService.java
@@ -0,0 +1,36 @@
+package kazi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MarriedCoupleService {
+    @Autowired
+    MarriedCoupleRepository mcRepository;
+
+    public MarriedCoupleService() {
+    }
+
+    public List<MarriedCouple> getMarriedCouple(String email) {
+
+        return mcRepository.findByMcemail(email);
+    }
+
+    public MarriedCouple getMcByemailAndPassword(String email, String password) {
+
+        return mcRepository.findByMcemailAndPassword(email, password);
+    }
+
+    public Iterable<MarriedCouple> getAll() {
+        return mcRepository.findAll();
+    }
+
+    public String addmc(MarriedCouple temp) {
+        if (!getMarriedCouple(temp.mcemail).isEmpty()) return "Couple Exists";
+        mcRepository.save(temp);
+        return "Success";
+    }
+
+}
