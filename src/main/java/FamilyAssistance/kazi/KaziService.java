08ded470850c4de04f38afe08bf3d027103e2b11
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-10-15 21:35:35 +0600

    Kazi Profile get and edit problem solution try

--- src/main/java/FamilyAssistance/kazi/KaziService.java
+++ src/main/java/FamilyAssistance/kazi/KaziService.java
@@ -5,46 +5,43 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KaziService {
-	
-	@Autowired
-	KaziRepository kaziRepository;
 
-	public KaziService() {
-	}
+    @Autowired
+    KaziRepository kaziRepository;
 
-	public Kazi getKazi(String license) {
+    public KaziService() {
+    }
 
-		return kaziRepository.findByKaziLicenceNumber(license);
-	}
+    public Kazi getKazi(String license) {
 
-	public Kazi getKaziByUserNameandPassword(String email,String password){
+        return kaziRepository.findByKaziLicenceNumber(license);
+    }
 
-		return kaziRepository.findByKaziEmailAndKaziPassword(email,password);
-	}
+    public Kazi getKaziByUserNameandPassword(String email, String password) {
 
-	
-	public Iterable<Kazi> getAll(){
-		return kaziRepository.findAll();
-	}
+        return kaziRepository.findByKaziEmailAndKaziPassword(email, password);
+    }
 
-	public String addkazi(Kazi temp) {
-        try {
-            if (!(getKazi(temp.kaziLicenceNumber).equals(new Kazi()))) {
-                return "Kazi Exists";
-            }
-        } catch (Exception e) {
-			
+
+    public Iterable<Kazi> getAll() {
+        return kaziRepository.findAll();
+    }
+
+    public String addkazi(Kazi temp) {
+
+        if (!(getKazi(temp.kaziLicenceNumber).equals(new Kazi()))) {
+            return "Kazi Exists";
         }
 
-		kaziRepository.save(temp);
-		return "Success";
-	}
+        kaziRepository.save(temp);
+        return "Success";
+    }
 
-	public void updateKazi(Kazi temp,Long id) {
+    public void updateKazi(Kazi temp, Long id) {
 
-	}
+    }
 
-	public void deleteKazi(Kazi temp, Long id) {
+    public void deleteKazi(Kazi temp, Long id) {
 
-	}
+    }
 }
