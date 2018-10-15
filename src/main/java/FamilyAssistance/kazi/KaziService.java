add1d4380d23b189e4baaee31ad06fd2f6a2670f
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-10-15 19:38:20 +0600

    Kazi Profile get and edit problem solution try

--- src/main/java/FamilyAssistance/kazi/KaziService.java
+++ src/main/java/FamilyAssistance/kazi/KaziService.java
@@ -28,7 +28,7 @@ public class KaziService {
 	}
 
 	public String addkazi(Kazi temp) {
-        if (!getKazi(temp.kaziLicenceNumber).getKaziLicenceNumber().equals("")) return "Kazi Exists";
+        if (getKazi(temp.kaziLicenceNumber).getKaziLicenceNumber() != null) return "Kazi Exists";
 		kaziRepository.save(temp);
 		return "Success";
 	}
