09c0c427b219218d9c3ed8e6b3efc3ff31ef3368
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-10-16 19:15:19 +0600

    File upload, download included

---
+++ src/main/java/FamilyAssistance/file/FileStorageProperties.java
@@ -0,0 +1,13 @@
+package FamilyAssistance.file;
+
+public class FileStorageProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
