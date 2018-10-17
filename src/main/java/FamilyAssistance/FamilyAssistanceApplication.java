1f40bba4bd554f0bc353f11edb999a64aeb3ab83
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-10-17 12:14:29 +0600

    Added this in main program
    @SpringBootApplication
    @EnableConfigurationProperties({
            FileStorageProperties.class
    })

--- src/main/java/FamilyAssistance/FamilyAssistanceApplication.java
+++ src/main/java/FamilyAssistance/FamilyAssistanceApplication.java
@@ -1,16 +1,24 @@
 package FamilyAssistance;
 
+import FamilyAssistance.file.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan({"FamilyAssistance.faq", "FamilyAssistance.kazi", "FamilyAssistance.marriageRegistration", "FamilyAssistance.marriedCouple", "FamilyAssistance.person", "FamilyAssistance.file"})
 public class FamilyAssistanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FamilyAssistanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyAssistanceApplication.class, args);
+    }
 }
