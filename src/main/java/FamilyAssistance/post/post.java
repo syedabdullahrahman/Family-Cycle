2f6a1bd1ba86867ba1561af3a47bece9dfb8616f
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-09 11:38:09 +0600

    Post concept is added

--- src/main/java/FamilyAssistance/post/post.java
+++ src/main/java/FamilyAssistance/post/post.java
@@ -1,14 +1,19 @@
 package FamilyAssistance.post;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class post {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "PR_KEY")
     String id;
 
     String title;
