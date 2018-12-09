f36b1fd0ba57ca7b35ee391350e7776fcd7c2c66
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-09 11:27:36 +0600

    Post concept is added

--- src/main/java/FamilyAssistance/post/postController.java
+++ src/main/java/FamilyAssistance/post/postController.java
@@ -1,9 +1,10 @@
 package FamilyAssistance.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -17,10 +18,10 @@ public class postController {
         return ps.getAll();
     }
 
-    @RequestMapping(value = "/findpost", params = {"user_id", "stage"})
-    public List<post> getKazi(@RequestParam("user_id") String user_id, @RequestParam("stage") String stage) {
-        return ps.getPostbyUser_idAndStage(user_id, stage);
-    }
+    //@RequestMapping(value = "/findpost", params = {"user_id", "stage"})
+    //public List<post> getKazi(@RequestParam("user_id") String user_id, @RequestParam("stage") String stage) {
+    //return ps.getPostbyUser_idAndStage(user_id, stage);
+    //}
 
     @RequestMapping(method = RequestMethod.POST, value = "/addpost")
     //specify the method otherwise default GET method work
