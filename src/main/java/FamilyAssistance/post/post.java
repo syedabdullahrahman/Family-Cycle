6cd4a4b2fddca12a5f02a1f41b3ae924dc7f2760
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-09 12:05:54 +0600

    Post concept is added

---
+++ src/main/java/FamilyAssistance/post/post.java
@@ -0,0 +1,67 @@
+package FamilyAssistance.post;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    String title;
+    String description;
+
+    String stage;
+    String user_id;
+
+    public post(String title, String description, String stage, String user_id) {
+        this.title = title;
+        this.description = description;
+        this.stage = stage;
+        this.user_id = user_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ src/main/java/FamilyAssistance/post/postRepository.java
@@ -0,0 +1,14 @@
+package FamilyAssistance.post;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface postRepository extends CrudRepository<post, String> {
+
+
+    public List<post> findByUser_idAndAndStage(String user_id, String stage);
+
+}
