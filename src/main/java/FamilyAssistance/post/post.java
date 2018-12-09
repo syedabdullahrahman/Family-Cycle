8bee438718ab89e3b469e6720853147bfa56c6f8
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-09 11:22:40 +0600

    Post concept is added

--- src/main/java/FamilyAssistance/post/post.java
+++ src/main/java/FamilyAssistance/post/post.java
@@ -1,12 +1,14 @@
 package FamilyAssistance.post;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
     String id;
 
     String title;
