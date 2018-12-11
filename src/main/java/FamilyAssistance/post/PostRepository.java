675040f734128769ceb4a20b3eb34a3ef0b16848
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-11 16:33:51 +0600

    Post concept is added

--- src/main/java/FamilyAssistance/post/PostRepository.java
+++ src/main/java/FamilyAssistance/post/PostRepository.java
@@ -1,12 +1,11 @@
 package FamilyAssistance.post;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends Repository {
 
     List<Post> findByUser_idAndAndStage(String user_id, String stage);
 }
