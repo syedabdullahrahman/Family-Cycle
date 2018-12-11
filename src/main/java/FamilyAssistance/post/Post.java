af68c9cfbefc8e658d92256ae64cef1f97f41ca7
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-11 13:35:26 +0600

    Post concept is added

--- src/main/java/FamilyAssistance/post/Post.java
+++ src/main/java/FamilyAssistance/post/Post.java
@@ -1,24 +1,32 @@
 package FamilyAssistance.post;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "serial")
     Long id;
-
     String title;
     String description;
-
     String stage;
     String user_id;
 
     public Post() {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Post(String title, String description, String stage, String user_id) {
--- src/main/java/FamilyAssistance/post/PostService.java
+++ src/main/java/FamilyAssistance/post/PostService.java
@@ -4,6 +4,8 @@ package FamilyAssistance.post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
@@ -22,4 +24,7 @@ public class PostService {
         return "Success";
     }
 
+    public List<Post> getPost(String user_id, String stage) {
+        return (List<Post>) postRepository.findAll();
+    }
 }
