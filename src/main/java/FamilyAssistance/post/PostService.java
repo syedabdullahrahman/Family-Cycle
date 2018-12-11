e5bb70837a6d0773e62228fb9de59bf25913c52c
syedabdullarahman <syedabdullahrahman@gmail.com>
2018-12-11 12:54:22 +0600

    Post concept is added

--- src/main/java/FamilyAssistance/post/PostService.java
+++ src/main/java/FamilyAssistance/post/PostService.java
@@ -4,8 +4,6 @@ package FamilyAssistance.post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class PostService {
 
@@ -24,7 +22,4 @@ public class PostService {
         return "Success";
     }
 
-    public List<Post> getPost(String user_id, String stage) {
-        return postRepository.findByUser_idAndAndStage(user_id, stage);
-    }
 }
